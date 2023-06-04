ae46549ffca0c4096d0491095672a8f9830c3ff7
wilsonz0 <105890511+wilsonz0@users.noreply.github.com>
2023-06-04 00:41:17 -0400

    created a way for the current and average temperature to be diplayed

--- script.js
+++ script.js
@@ -11,5 +11,24 @@ function findTemp() {
 
    fetch("https://api.open-meteo.com/v1/forecast?latitude=" + lat + "&longitude=" + long + "&hourly=temperature_2m&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&forecast_days=1")
    .then(response => response.json())
-   .then(data => console.log(data))
+   .then(data => displayTemp(data))
+}
+
+function displayTemp(data) {
+   let today = new Date();
+   let temp = data.hourly.temperature_2m
+   let tempAvg = temp.reduce((a, b) => a + b) / temp.length
+   let time = data.hourly.time
+   
+   document.getElementById("averageTemp").innerHTML = tempAvg
+
+   // find the current time by parsing and then verifying it with our current time
+   for (let i = 0; i < time.length; i++) {
+      let curr = time[i]
+      let currParsed = parseInt(curr.substring(curr.length - 5, curr.length - 3))
+
+      if (today.getHours() == currParsed) {
+         document.getElementById("currentTemp").innerHTML = temp[i] 
+      }
+   }
 }
